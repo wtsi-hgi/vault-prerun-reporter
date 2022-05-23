3f6cd39c9a4ed5312f842e4cbe0c12f616b29b76
michael-grace <michael.dp.grace@outlook.com>
2022-05-23 13:47:33 +0100

    change to normal queue - it doens't need long queue

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 source /software/hgi/installs/vault/prerun-reporter/.venv/bin/activate
 
-bsub -o /nfs/hgi/vault/pre_reports/%J.md -e /nfs/hgi/vault/pre_reports/%J.err -G hgi -q long -R "select[mem>3000] rusage[mem=3000]" -M 3000 "python /software/hgi/installs/vault/prerun-reporter/report.py $1"
+bsub -o /nfs/hgi/vault/pre_reports/%J.md -e /nfs/hgi/vault/pre_reports/%J.err -G hgi -R "select[mem>3000] rusage[mem=3000]" -M 3000 "python /software/hgi/installs/vault/prerun-reporter/report.py $1"
